80fbceeb9d144ad2c8ff4d921042ff427b9b343d
Frank <frank.snelling03@gmail.com>
2024-01-22 13:29:03 +0000

    added main user controlled plane class and created an instance of it in the main loop. the plane can be controlled using simple up, down, left and right keys on keyboard

--- main.py
+++ main.py
@@ -1,5 +1,34 @@
 import pygame
 
+class plane(object):
+    """This class is for all of the planes in the game"""
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.width = 150
+        self.height = 52
+
+class user_plane(plane):
+    """This class is specifically for the user controlled plane"""
+
+    image = pygame.image.load('user_plane.png') # upload main fighter
+
+    def __init__(self, x, y, velocity):
+        plane.__init__(self, x, y) # call superclass init method
+        self.velocity = velocity
+        self.image = pygame.transform.scale(self.image, (self.width, self.height)) # change plane size
+        self.image = pygame.transform.flip(self.image, True, False) # flip image
+
+    def draw(self, win):
+        win.blit(self.image, (self.x, self.y))
+
+
+def redraw_game_window():
+    """This function redraws the game window between every frame"""
+    main_plane.draw(win)
+    pygame.display.update()
+
 pygame.init()
 screen_x = 1500
 screen_y = 1100
@@ -7,6 +36,7 @@ win = pygame.display.set_mode((screen_x, screen_y)) # creating window
 pygame.display.set_caption("World War Game") # creating title
 clock = pygame.time.Clock() 
 
+main_plane = user_plane(100, 100, 20)
 run = True # main loop
 while run:
     clock.tick(27) # frame rate
@@ -14,5 +44,19 @@ while run:
     for event in pygame.event.get(): # check for event
         if event.type == pygame.QUIT: # if user closes window
             run = False
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_LEFT] and main_plane.x > 0:
+        main_plane.x -= main_plane.velocity
+    elif keys[pygame.K_RIGHT] and main_plane.x < screen_x - main_plane.width:
+        main_plane.x += main_plane.velocity
+    
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_UP] and main_plane.y > 0:
+        main_plane.y -= main_plane.velocity
+    elif keys[pygame.K_DOWN] and main_plane.y < screen_y - main_plane.height:
+        main_plane.y += main_plane.velocity
+    
+    redraw_game_window()
 
 pygame.quit() # closes program once broken out of while loop
